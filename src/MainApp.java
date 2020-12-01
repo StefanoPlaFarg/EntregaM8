f037ee345097f83e3998421f915a0b220714d032
Stefano <stefano.pola.fargas@gmail.com>
2020-12-01 19:19:08 +0100

    Correcio d'errors realtius amb el metode system.out.format

--- src/MainApp.java
+++ src/MainApp.java
@@ -15,8 +15,8 @@ public class MainApp {
 		
 		Rocket rocket1 = new Rocket ("32WESSDS",3);
 		Rocket rocket2 = new Rocket ("LDSFJA32",6);
-		System.out.format("El codi del primer cohet és: %s%n i disposa de %d propulsors",rocket1.getIdCode(),rocket1.getNumberJetPropellers() );
-		System.out.format("El codi del segon cohet és: %s%n i disposa de %d propulsors",rocket2.getIdCode(),rocket2.getNumberJetPropellers() );
+		System.out.format("El codi del primer cohet és: %s i disposa de: %d propulsors %n",rocket1.getIdCode(),rocket1.getNumberJetPropellers() );
+		System.out.format("El codi del segon cohet és: %s i disposa de: %d propulsors",rocket2.getIdCode(),rocket2.getNumberJetPropellers() );
 
 	}
 
