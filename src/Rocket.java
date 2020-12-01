c1252773e5de5f2dea16c3dccf51b7e66ff7d2e6
Stefano <stefano.pola.fargas@gmail.com>
2020-12-01 18:51:55 +0100

    Creacio classe Rocket i els seus métodes

--- src/Rocket.java
+++ src/Rocket.java
@@ -10,6 +10,35 @@ import java.util.List;
  *
  */
 public class Rocket {
+	
+	private String idCode;
+	private List<JetPropeller> listPropellers = new ArrayList<JetPropeller>();
+	
+	
+	public Rocket() {
+
+	}
+	
+	public Rocket(String idCode) {
+    this.idCode=idCode;
+	}
+	
+	public Rocket(String idCode, int numJetPropellers) {
+		this.idCode=idCode;
+		this.idCode=idCode;
+	}
+	
+	public void setIdCode(String idCode) {
+		this.idCode=idCode;
+	}
+	
+	
+	public void addJetPropeller() {
+		listPropellers.add(new JetPropeller());
+	}
 		
+	public int getNumberJetPropellers() {
+		 return listPropellers.size();
+	}
 
 }
